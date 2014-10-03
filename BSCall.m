26ab75a4b73142e9c653dd8a04e814038c6a39c4
Matthew Fieger <fieger.matthew@gmail.com>
2014-10-03 00:26:44 -0400

    bs call and put

---
+++ BSCall.m
@@ -0,0 +1,46 @@
+function [ return_val ] = BSCall(S, K, r, q, vol, T)
+% Calculates the price of a European call option
+% based on the Black-Scholes option pricing model.
+
+    % S = Spot price of the underlying asset
+    % K = Strike price of the option
+    % r = Current risk free rate, annual continuous compounding
+    % q = Dividend yield
+    % vol = Standard deviaton of returns of the underlying asset
+    % T = Time to experitation
+    
+    d_1 = (log(S/K)+(r-q+vol^2/2)*T)/(vol*sqrt(T));
+    d_2 =  d_1 - vol*sqrt(T);
+
+    Nd_1 = normcdf(d_1,0,1);
+    Nd_2 = normcdf(d_2,0,1);
+
+    c = S*exp(-q*T)*Nd_1 - K*exp(-r*T)*Nd_2;
+
+    return_val = c;
+
+end
+
+function [ return_val ] = BSPut(S, K, r, q, vol, T)
+% Calculates the price of a European call option
+% based on the Black-Scholes option pricing model.
+
+    % S = Spot price of the underlying asset
+    % K = Strike price of the option
+    % r = Current risk free rate, annual continuous compounding
+    % q = Dividend yield
+    % vol = Standard deviaton of returns of the underlying asset
+    % T = Time to experitation
+    
+    d_1 = (log(S/K)+(r-q+vol^2/2)*T)/(vol*sqrt(T));
+    d_2 =  d_1 - vol*sqrt(T);
+
+    Nd_1 = normcdf(-d_1,0,1);
+    Nd_2 = normcdf(-d_2,0,1);
+
+    p = -S*exp(-q*T)*Nd_1 + K*exp(-r*T)*Nd_2;
+
+    return_val = p;
+
+end
+
